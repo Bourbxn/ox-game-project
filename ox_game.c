7932f448435740b7e97ecd4cd85fed96dc215edc
Bourbxn <jakkapat97@gmail.com>
2022-09-22 03:43:10 +0700

    add menu pvp 60%

--- ox_game.c
+++ ox_game.c
@@ -1,104 +1,202 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_board(){
+//define function
+void menu_main();
+void menu_pvc();
+void menu_pvp();
+void menu_exit();
+
+//render
+void render_menu(){
+	printf(" ,-----.,--.   ,--.     ,----.\n");
+	printf("'  .-.  '\\  `.'  /     '  .-./    ,--,--.,--,--,--. ,---.\n");
+	printf("|  | |  | .'    \\      |  | .---.' ,-.  ||        || .-. :\n");
+	printf("'  '-'  '/  .'.  \\     '  '--'  |\\ '-'  ||  |  |  |\\   --.\n");
+	printf(" `-----''--'   '--'     `------'  `--`--'`--`--`--' `----' \n");
+	printf("===========================================================\n");
+	printf("Select Menu\n");
+	printf("[1] Player vs Computer\n");
+	printf("[2] Player vs Player\n");
+	printf("[3] Exit\n");
+	printf("Choose: ");
+}
+
+void render_board(char ox[6][7]){
   printf("  A   B   C   D   E   F   G\n");
-  for(int i=0;i<13;i++){
-    for(int j=0;j<7;j++){
-      if(i%2==0 && j==0){
-        printf("|---|");
-      }
-      else if(i%2==0 && j!=0){
-        printf("---|");
-        
-      }
-      else if(i%2==1 && j==0){
-        printf("| %c |",' ');
-      }
-      else if(i%2==1 && j!=0){
-        printf(" %c |",' ');
-      }
-      if(j==6){
-        printf("\n");
-      }
-    }
-  }
-  printf("\n");
+	for(int i=0;i<6;i++){
+		for(int j=0;j<7;j++){
+			for(int k=0;k<7 && j==0;k++){
+				if(k==0) printf("|");
+				printf("---|");
+				if(k==6) printf("\n");
+			}
+			if(j==0) printf("|");
+			printf(" %c |",ox[i][j]);
+		}
+		printf("\n");
+	}
+	for(int i=0;i<7;i++){
+		if(i==0) printf("|");
+		printf("---|");
+		if(i==6) printf("\n");
+	}
+	for(int i=0;i<29;i++){
+		printf("=");
+		if(i==28) printf("\n");
+	}
 }
 
-void play_pp(){
-  system("clear");
-  char go;
-  print_board();
-  printf("Where do you want to go(A-G)?\n>>");
-  scanf("%c",&go);
-}
-
-int menu(){
-  int choice;
-  printf(" ,-----.,--.   ,--.     ,----.\n");
-  printf("'  .-.  '\\  `.'  /     '  .-./    ,--,--.,--,--,--. ,---.\n");
-  printf("|  | |  | .'    \\      |  | .---.' ,-.  ||        || .-. :\n");
-  printf("'  '-'  '/  .'.  \\     '  '--'  |\\ '-'  ||  |  |  |\\   --.\n");
-  printf(" `-----''--'   '--'     `------'  `--`--'`--`--`--' `----' \n");
-  printf("===========================================================\n");
+void render_pvp_result(char ox[6][7],char pl[1000]){
+  render_board(ox);
+  printf("!!! The Winner is %s !!!\n\n",pl);
   printf("Select Menu\n");
-  printf("[1] Player vs Computer\n");
-  printf("[2] Player vs Player\n");
-  printf("[3] Exit\n");
-  printf("Choose: ");
-  scanf("%d",&choice);
-  if(choice==1){
-    system("clear");
-    choice = 2;
-  }
-  else if(choice==2){
-    system("clear");
-    choice = 3;
-  }
-  else if(choice==3){
-    system("clear");
-    exit(0);
-  }
-  return choice;
+	printf("[1] Restart\n");
+	printf("[2] Menu\n");
+	printf("Choose: ");
 }
 
-int menu_pp(){
-  char name1[1000],name2[1000];
-  print_board();
-  printf("Player 1: ");
-  scanf("%s",name1);
-  printf("Player 2: ");
-  scanf("%s",name2);
-  while(1){
-    play_pp();
+//game engine
+int is_bingo(char ox[6][7]){
+  //check col
+	int isBingo = 0;
+  for(int i=0;i<7 && !isBingo;i++){
+    for(int j=5;j>=3;j--){
+      if(ox[j][i]==ox[j-1][i] &&
+          ox[j-1][i]==ox[j-2][i] &&
+          ox[j-2][i]==ox[j-3][i] &&
+          ox[j][i]!=' ' && ox[j-1][i]!= ' ' &&
+          ox[j-2][i]!=' ' && ox[j-3][i] != ' ')
+        isBingo = 1;
+    }
+  }	
+  //check row
+  for(int i=5;i>=0 && !isBingo;i--){
+    for(int j=0;j<4;j++){
+      if(ox[i][j]==ox[i][j+1] &&
+          ox[i][j+1]==ox[i][j+2] &&
+          ox[i][j+2]==ox[i][j+3] &&
+          ox[i][j]!=' ' && ox[i][j+1]!= ' ' &&
+          ox[i][j+2]!=' ' && ox[i][j+3] != ' ')
+        isBingo = 1;
+    }
   }
-  return 0;
+  //check left to right diagonal
+  for(int i=5;i>=3 && !isBingo;i--){
+    for(int j=0;j<4;j++){
+      if(ox[i][j]==ox[i-1][j+1] &&
+          ox[i-1][j+1]==ox[i-2][j+2] &&
+          ox[i-2][j+2]==ox[i-3][j+3] &&
+          ox[i][j]!=' ' && ox[i-1][j+1]!= ' ' &&
+          ox[i-2][j+2]!=' ' && ox[i-3][j+3] != ' ')
+        isBingo = 1;
+    }
+  }
+  //check right to left diagonal
+  for(int i=5;i>=3 && !isBingo;i--){
+    for(int j=3;j<=6;j++){
+      if(ox[i][j]==ox[i-1][j-1] &&
+          ox[i-1][j-1]==ox[i-2][j-2] &&
+          ox[i-2][j-2]==ox[i-3][j-3] &&
+          ox[i][j]!=' ' && ox[i-1][j-1]!= ' ' &&
+          ox[i-2][j-2]!=' ' && ox[i-3][j-3] != ' ')
+        isBingo = 1;
+    }
+  }
+ return isBingo;
 }
 
-int menu_pc(){
-  return 0;
+//sub menu
+void menu_pvp_result(char ox[6][7],char pl[1000]){
+  system("clear");
+  int select;
+  render_pvp_result(ox,pl);
+  scanf("%d",&select);
+  if(select==1){
+    menu_pvp();
+  }
+  else if(select==2){
+    menu_main();
+  }
 }
 
+//menu
+void menu_main(){
+  system("clear");
+	int select;
+	render_menu();
+	scanf("%d",&select);
+	if(select==1){
+		menu_pvc();
+	}
+	else if(select==2){
+		menu_pvp();
+	}
+	else if(select==3){
+		menu_exit();
+	}
+}
+
+void menu_pvc(){
+
+}
+
+void menu_pvp(){
+	system("clear");
+	char ox[6][7];
+	char p1[1000],p2[1000],plwin[1000];
+	int round = 0;
+	for(int i=0;i<6;i++){
+		for(int j=0;j<7;j++){
+			ox[i][j] = ' ';
+		}
+	}
+	render_board(ox);
+	printf("Player1: ");
+	scanf("%s",p1);
+	printf("Player2: ");
+	scanf("%s",p2);
+	while(1){
+		system("clear");
+		char pick[1],oxb;
+		int drop;
+		render_board(ox);
+		if(round==0) printf("%s, your turn!\n\n",p1);
+		else printf("%s, your turn!\n\n",p2);
+		printf("Where do you want to go(A-G)?\n");
+		scanf("%s",pick);
+		drop = pick[0]-'A';
+		if(round==0) oxb='X';
+		else oxb='O';
+		for(int i=5;i>=0;i--){
+			if(ox[i][drop]==' '){
+				ox[i][drop]=oxb;
+				break;
+			}
+		}
+    if(is_bingo(ox)){
+      if(round==0) strcpy(plwin,p1);
+      else if(round==1) strcpy(plwin,p2);
+      break;
+    }
+		round=!round;
+	}
+  menu_pvp_result(ox,plwin);
+}
+
+void menu_exit(){
+	system("clear");
+	exit(0);
+}
+
+//core
 void ox_game(){
-  system("clear");
-  int choice = 1;
-  while(1){
-    if(choice==1){
-      choice = menu();
-    }
-    else if(choice==2){
-      choice = menu_pc();
-    }
-    else if(choice==3){
-      choice = menu_pp();
-    }
-  } 
+	menu_main();
 }
 
+//main
 int main(){
-  system("clear");
-  ox_game();
-  return 0;
+	ox_game();
+	return 0;
 }
-                                                
--- test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	char chars[3][3] = {};
-	printf("%c",chars[0][1]);
-}
