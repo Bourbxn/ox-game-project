7d9a2152125a582e3e47539d8e14f8d27f4d172c
Bourbxn <jakkapat97@gmail.com>
2022-09-23 14:11:31 +0700

    add pvc backup 90%

---
+++ test.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+int main(){
+	int num[7],len;
+	num[0] = 1;
+	num[1] = 2;
+	num[2] = 3;
+	num[3] = 4;
+	num[4] = 5;
+	for(int i=0;i<7;i++){
+		printf("%d\n",num[i]);
+	}
+	printf("%d",len);
+	return 0;
+}
